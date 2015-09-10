41a56b5671016abfcf3b2b52940282baa1efa64b
w0mTea <w0mT3a@gmail.com>
2015-09-10 14:23:46 +0800

    Complete the one-step evaluation function

--- Simple1.hs
+++ Simple1.hs
@@ -41,8 +41,30 @@ data Term = TmTrue SourceInfo
           | TmSucc SourceInfo Term
           | TmPred SourceInfo Term
           | TmIsZero SourceInfo Term
+          deriving (Eq, Show)
 
 isNumericVal :: Term -> Bool
 isNumericVal (TmZero _) = True
 isNumericVal (TmSucc _ t) = isNumericVal t
 isNumericVal _ = False
+
+isVal :: Term -> Bool
+isVal (TmTrue _) = True
+isVal (TmFalse _) = True
+isVal t = isNumericVal t
+
+oneStepEval :: Term -> Term
+oneStepEval (TmIf _ (TmTrue _) t _) = t
+oneStepEval (TmIf _ (TmFalse _) _ f) = f
+oneStepEval (TmIf info c t f) = TmIf info c' t f
+  where c' = oneStepEval c
+oneStepEval (TmSucc info t) = TmSucc info t'
+  where t' = oneStepEval t
+oneStepEval (TmPred _ z@(TmZero _)) = z
+oneStepEval (TmPred _ (TmSucc _ t)) = t
+oneStepEval (TmPred info t) = TmPred info t'
+  where t' = oneStepEval t
+oneStepEval (TmIsZero info (TmZero _)) = TmTrue info
+oneStepEval (TmIsZero info (TmSucc _ _)) = TmFalse info
+oneStepEval (TmIsZero info t) = TmIsZero info t'
+  where t' = oneStepEval t
