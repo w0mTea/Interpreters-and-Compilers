66df7f004a9368030da71e10afe3d9386c96d510
w0mTea <w0mT3a@gmail.com>
2015-10-08 00:19:13 +0800

    Fix a some small bugs in parser

--- Lambda-Calculus/Lambda.hs
+++ Lambda-Calculus/Lambda.hs
@@ -1,9 +1,9 @@
 import Text.Parsec
-import Text.Parsec.Combinator (between, sepBy1, chainr1)
-import Data.List (elemIndex)
+import Text.Parsec.Combinator (between, chainl1)
+import Data.List (isPrefixOf)
 import System.IO
 import System.Environment
-import Data.List (isPrefixOf)
+import Control.Monad (liftM)
 
 data Info = Info {row :: Int, col :: Int}
           deriving Show
@@ -33,20 +33,20 @@ name = do
   let firstLetters = ['A' .. 'Z'] ++ ['a' .. 'z'] ++ ['_']
       identLetters = firstLetters ++ ['0' .. '9']
   spaces
-  fst <- oneOf firstLetters
-  rest <- many $ oneOf identLetters
-  tail <- many $ char '\''
+  firstL <- oneOf firstLetters
+  restL <- many $ oneOf identLetters
+  tailL <- many $ char '\''
   spaces
-  return $ (fst : rest) ++ tail
+  return $ (firstL : restL) ++ tailL
 
 parseAbs :: LCParser
 parseAbs = do
   spaces
   pos <- getPosition
-  char '\\'
+  _ <- char '\\'
   n <- name
   modifyState ((n, EmptyBinding) :)
-  char '.'
+  _ <- char '.'
   t <- parseTerm
   modifyState tail
   return $ TmAbs (infoFrom pos) n t
@@ -59,7 +59,7 @@ parseVar = do
 
 findVar :: String -> Context -> LCParser
 findVar v ctx = case indexInCtx v ctx of
-  Nothing -> (fail $ "Variable " ++ v ++ " cannot be found") <?> "a bouned variable"
+  Nothing -> fail ("Variable \"" ++ v ++ "\" cannot be found") <?> "a bouned variable"
   Just n  -> do
     pos <- getPosition
     return $ TmVar (infoFrom pos) n (length ctx)
@@ -72,13 +72,18 @@ indexInCtx name ctx = func name ctx 0
                                 | otherwise = func n xs (i+1)
 
 parens :: Parsec String u a -> Parsec String u a
-parens = between (char '(') (char ')')
+parens p = do
+    spaces
+    _ <- char '('
+    t <- p
+    _ <- char ')'
+    spaces
+    return t
 
 parseNonApp :: LCParser
 parseNonApp =  parens parseTerm
-      <|> parseAbs
-      <|> parseVar
-
+           <|> parseAbs
+           <|> parseVar
 
 parseTerm :: LCParser
 parseTerm = chainl1 parseNonApp $ do
@@ -88,14 +93,13 @@ parseTerm = chainl1 parseNonApp $ do
 
 lcParse :: String -> Either ParseError Term
 lcParse = parseWith parseTerm
-
 --}
 
 --{ Core functions
 
 tmMap :: (Int -> Term -> Term) -> Term -> Term
-tmMap f t = func 0 t
-  where func c t1@(TmVar _ _ _) = f c t1
+tmMap f = func 0
+  where func c t1@(TmVar {}) = f c t1
         func c (TmAbs info name t1) = TmAbs info name $ func (c + 1) t1
         func c (TmApp info t1 t2) = TmApp info (func c t1) (func c t2)
 
@@ -118,16 +122,16 @@ tmAppAbs t1 t2 = tmShift (-1) $ tmSubst 0 (tmShift 1 t1) t2
 --{ Evaluatino functions
 
 isVal :: Term -> Bool
-isVal (TmAbs _ _ _) = True
+isVal (TmAbs {}) = True
 isVal _ = False
 
 eval1 :: Term -> Maybe Term
-eval1 (TmApp info t@(TmAbs _ _ _) v2)
+eval1 (TmApp info t@(TmAbs {}) v2)
   | isVal v2 = return $ tmAppAbs t v2
   | otherwise = let v2' = eval1 v2 in
-    v2' >>= return . TmApp info t
+    liftM (TmApp info t) v2'
 eval1 (TmApp info t1 t2) = let t1' = eval1 t1 in
-  t1' >>= return . flip (TmApp info) t2
+  liftM (flip (TmApp info) t2) t1'
 eval1 _ = Nothing
 
 eval :: Term -> Term
@@ -143,8 +147,8 @@ eval t = let t' = eval1 t in
 -- rebuild context and print
 printTerm :: Context -> Term -> String
 printTerm ctx (TmAbs _ name t1) = let (ctx', name') = pickFreshName ctx name in
-  "(λ" ++ name' ++ ". " ++ printTerm ctx' t1 ++ ")"
-printTerm ctx (TmApp _ t1 t2) = "(" ++ printTerm ctx t1 ++ printTerm ctx t2 ++ ")"
+  "(λ" ++ name' ++ "." ++ printTerm ctx' t1 ++ ")"
+printTerm ctx (TmApp _ t1 t2) = "(" ++ printTerm ctx t1 ++ " " ++ printTerm ctx t2 ++ ")"
 printTerm ctx (TmVar info x len) = if length ctx == len
                                   then indexToName info ctx x
                                   else "[bad index]"
@@ -154,17 +158,35 @@ pickFreshName ctx name = (ctx', name')
   where (nc, ns) = foldr func (0, []) ctx
         name' = newName nc ns
         ctx' = (name, EmptyBinding) : ctx
-        func (n, _) (c, lst) = if n == name
-                               then (c+1, lst)
-                               else if name `isPrefixOf` n
-                                    then (c, n : lst)
-                                    else (c, lst)
-        newName c ns = let n' = name ++ (show c) in
-          if n' `elem` ns then newName (c+1) ns else n'
-    
+        func (n, _) (c, lst)
+            | n == name = (c + 1, lst)
+            | name `isPrefixOf` n = (c, n : lst)
+            | otherwise = (c, lst)
+        newName c ns' = let n' = if c > 0 then name ++ show c else name in
+          if n' `elem` ns' then newName (c+1) ns' else n'
+
 indexToName :: Info -> Context -> Int -> String
-indexToName info ctx index = fst $ f ctx index
-  where f (x:xs) 0 = x
+indexToName _ ctx index = fst $ f ctx index
+  where f (x:_) 0 = x
         f (x:xs) n = f xs (n-1)
 
 --}
+
+main :: IO [()]
+main = do
+  args <- getArgs
+  mapM runFile args
+
+runFile :: String -> IO ()
+runFile path = do
+  s <- readFile path
+  let term = lcParse s
+  case term of
+    Left e -> print e
+    Right t -> evalTerm t
+
+evalTerm :: Term -> IO ()
+evalTerm t = let t' = eval t in
+  if isVal t'
+  then putStrLn $ printTerm [] t'
+  else putStrLn "Evaluating Error"
